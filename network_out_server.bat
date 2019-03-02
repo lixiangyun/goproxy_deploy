610d0916ece1931203f8b923f08f55331af16f50
lixiangyun <linimbus@126.com>
2019-03-02 17:03:48 +0800

    update

--- network_out_server.bat
+++ network_out_server.bat
@@ -1,4 +1,5 @@
-proxy.exe bridge -p ":38000" -C ./proxy.crt -K ./proxy.key
-proxy.exe server -r ":33389@:3389" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
-proxy.exe server -r ":30080@:80" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
-proxy.exe server -r ":30021@:21" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
+start proxy.exe bridge -p ":38000" -C ./proxy.crt -K ./proxy.key
+start proxy.exe server -r ":33389@:3389" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
+start proxy.exe server -r ":30080@:80" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
+start proxy.exe server -r ":30021@:21" -P "127.0.0.1:38000" -C ./proxy.crt -K ./proxy.key
+pause
