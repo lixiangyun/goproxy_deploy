6b1bfd1e6c07d4862675a0e878c2a1a80bee5f1c
lixiangyun <linimbus@126.com>
2019-04-25 22:54:41 +0800

    fix-bug

--- http_proxy_server.sh
+++ http_proxy_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/goproxy_deploy
 killall goproxy_basic
-killall tcpproxy
+killall vchannel
 nohup ./goproxy_basic -addr :8080 &
 nohup ./goproxy_basic -addr :8081 &
 nohup ./goproxy_basic -addr :8082 &
@@ -12,5 +12,5 @@ nohup ./goproxy_basic -addr :8086 &
 nohup ./goproxy_basic -addr :8087 &
 nohup ./goproxy_basic -addr :8088 &
 nohup ./goproxy_basic -addr :8089 &
-nohup ./tcpproxy -config config_server.yaml &
+nohup ./vchannel -config config_channel.yaml -mode server &
 exit 0
