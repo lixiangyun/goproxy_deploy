a4c695cd3d3f264e879957d18e2089957f20ee02
lixiangyun <linimbus@126.com>
2020-01-04 13:51:03 +0800

    update

--- http_proxy_client.bat
+++ http_proxy_client.bat
@@ -1,4 +1,4 @@
 #start proxy.exe http -t tcp -p ":8080" -T tcp -P "127.0.0.1:8000"
-start tcpproxy.exe -config config_client.yaml
-
+tcpproxy.exe -config config_client.yaml
+pause
 #start vchannel.exe -config config_channel.yaml -mode client
--- http_proxy_server.sh
+++ http_proxy_server.sh
@@ -2,9 +2,5 @@
 export PATH=$PATH:/usr/bin
 
 goproxy_basic -addr 127.0.0.1:1000 &
-goproxy_basic -addr 127.0.0.1:1001 &
-goproxy_basic -addr 127.0.0.1:1002 &
-goproxy_basic -addr 127.0.0.1:1003 &
-goproxy_basic -addr 127.0.0.1:1004 &
 
 tcpproxy -config config_server.yaml
